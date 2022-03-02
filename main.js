9189c3ff8847d3f6b25528a15222c5522161e9af
Jed556 <jguiriba11@gmail.com>
2022-03-02 12:22:54 +0800

    Adjustments for testing

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ let input = "we need to search for other techniques"
 
 const PORT = 8080;
 
-fs.readFile('./monitor/index.html', function (err, html) {
+// fs.readFile('./monitor/index.html', function (err, html) {
     
-    if (err) throw err;
+//     if (err) throw err;
     
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(PORT);
-});
+//     http.createServer(function (request, response) {
+//         response.writeHeader(200, { "Content-Type": "text/html" });
+//         response.write(html);
+//         response.end();
+//     }).listen(PORT);
+// });
 
 network(input, data);
--- system/network.js
+++ system/network.js
@@ -22,6 +22,7 @@ function network(input, data) {
 
     if (config.continuous) {
         if (fs.existsSync(trainCache)) {
+            let tTime = new Date();
             const trainedData = JSON.parse(fs.readFileSync(trainCache));
             trainedNet.fromJSON(trainedData);
             result = trainedNet.train(trainingData, {
@@ -30,18 +31,19 @@ function network(input, data) {
                 learningRate: config.learningRate || 0.001,
                 momentum: config.momentum || null,
                 logPeriod: config.logPeriod || 10,
-                log: stats => console.log(stats),
+                log: stats => console.log(`${stats}, time: ${((new Date() - tTime) / 1000 / 60).toPrecision(4)} min`),
                 timeout: config.timeout || Infinity,
                 // callback: null,  callbackPeriod: 10,
             });
             try { fs.writeFileSync(trainCache, JSON.stringify(trainedNet.toJSON(), null, 2)) } catch (e) { console.log(e) };
         } else {
+            let tTime = new Date();
             network.train(trainingData, {
                 iterations: config.iterations || 2000,
                 learningRate: config.learningRate || 0.001,
                 errorThresh: config.errorThresh || 0.001,
                 logPeriod: config.logPeriod || 10,
-                log: stats => console.log(stats)
+                log: stats => console.log(`${stats}, time: ${((new Date() - tTime) / 1000 / 60).toPrecision(4)} min`),
             });
             try { fs.writeFileSync(trainCache, JSON.stringify(network.toJSON(), null, 2)) } catch (e) { console.log(e) };
         }
@@ -50,13 +52,14 @@ function network(input, data) {
             const networkData = JSON.parse(fs.readFileSync(trainCache));
             network.fromJSON(networkData);
         } else {
+            let tTime = new Date();
             result = network.train(trainingData, {
                 iterations: config.iterations || 2000,
                 errorThresh: config.errorThresh || 0.011,
                 learningRate: config.learningRate || 0.001,
                 momentum: config.momentum || null,
                 logPeriod: config.logPeriod || 10,
-                log: stats => console.log(stats),
+                log: stats => console.log(`${stats}, time: ${((new Date() - tTime) / 1000 / 60).toPrecision(4)} min`),
                 timeout: config.timeout || Infinity,
                 // callback: null,  callbackPeriod: 10,
             });
