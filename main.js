6fbbb1470da733d1c718ec476f9e80779947bf76
Jed556 <jguiriba11@gmail.com>
2022-03-03 01:39:00 +0800

    Fix CORS error

--- main.js
+++ main.js
@@ -5,17 +5,31 @@ const fs = require('fs');
 const data = require('./models/data.json');
 let input = ""
 
-const PORT = 8080;
+const PORT1 = 8080;
+const PORT2 = 8888;
 
 fs.readFile('./monitor/index.html', function (err, html) {
     
     if (err) throw err;
     
     http.createServer(function (request, response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.writeHeader(200, { "Content-Type": "text/html" });
         response.write(html);
         response.end();
-    }).listen(PORT);
+    }).listen(PORT1);
 });
 
 network(input, data);
+
+fs.readFile('./cache/history.json', function (err, html) {
+    
+    if (err) throw err;
+    
+    http.createServer(function (request, response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.writeHeader(200, { "Content-Type": "application/json" });
+        response.write(html);
+        response.end();
+    }).listen(PORT2);
+});
