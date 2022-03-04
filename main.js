2ca95d6a6fadd9805cbdf5c842f82acb971616fc
Jed556 <jguiriba11@gmail.com>
2022-03-04 16:18:07 +0800

    Updated history monitor

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const http = require('http');
 const fs = require('fs');
 
 const data = require('./models/data.json');
-let input = ""
+let input = "search"
 
 const PORT1 = 8080;
 const PORT2 = 8888;
 
 fs.readFile('./monitor/index.html', function (err, html) {
-    
+
     if (err) throw err;
-    
+
     http.createServer(function (request, response) {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.writeHeader(200, { "Content-Type": "text/html" });
@@ -22,14 +22,15 @@ fs.readFile('./monitor/index.html', function (err, html) {
 
 network(input, data);
 
-fs.readFile('./cache/history.json', function (err, html) {
-    
-    if (err) throw err;
-    
-    http.createServer(function (request, response) {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.writeHeader(200, { "Content-Type": "application/json" });
-        response.write(html);
-        response.end();
-    }).listen(PORT2);
-});
+if (fs.existsSync('./cache/history.json'))
+    fs.readFile('./cache/history.json', function (err, html) {
+
+        if (err) throw err;
+
+        http.createServer(function (request, response) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.writeHeader(200, { "Content-Type": "application/json" });
+            response.write(html);
+            response.end();
+        }).listen(PORT2);
+    });
